25e739582df934e1a1bbaac5874b57d1c45fd733
Pramoda Majhi <pramoda@Pramodas-MacBook-Pro.local>
2018-10-12 09:30:56 -0700

    Initial commit

---
+++ jsonfileusingpanda.py
@@ -0,0 +1,8 @@
+import json
+import pandas as pd
+
+with open('reviews_Health_and_Personal_Care.json') as f:
+   ## print(json.loads())
+    df = pd.DataFrame(json.loads(line) for line in f)
+    ## print(df)
+    df.to_csv('Output.csv', sep='\t', encoding='utf-8')
---
+++ twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contain the user credentials to access Twitter API
+ACCESS_TOKEN = "844217614796652544-daUwhNPDLKokvU8MBJIBLvVLo79JdqU"
+ACCESS_TOKEN_SECRET = "p5GPpBbW85Til9uZ1hJifG0vJOLP7wXbDeoBJwpb4REbx"
+CONSUMER_KEY = "whQuPaW3mioB1EVWeG2toJQLm"
+CONSUMER_SECRET = "rih4dNiz4jkLWz86XcMhaj8pCp5xnBaBtzAxXd1V0cyqThvVdh"
